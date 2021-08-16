7762ecca7d502cba051b74325da19ce37db3132d
Adam-Markevych <87534391+Adam-Markevych@users.noreply.github.com>
2021-08-16 11:13:00 +0300

    Add files via upload

---
+++ task4/script.js
@@ -0,0 +1,21 @@
+function clickColor(){
+    if(event.target.className == 'col1'){
+       event.target.style.color = "red";
+    }
+    if(event.target.className == 'col2'){
+        event.target.style.color = "yellow";
+    }
+    if(event.target.className == 'col3'){
+        event.target.style.color = "green";
+    }
+    if(event.target.className == 'col4'){
+        event.target.style.color = "blue";
+    }
+    if(event.target.className == 'col5'){
+        event.target.style.color = "orange";
+    }
+    if(event.target.className == 'col6'){
+        event.target.style.color = "gray";
+    }
+}
+
